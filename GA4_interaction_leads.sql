d6e6d33df5735b2ae28bc4fcb683197090decc4d
Nico <90333183+nmcontreras@users.noreply.github.com>
2023-10-17 17:07:10 +0200

    Update GA4_interaction_leads.sql

--- GA4_interaction_leads.sql
+++ GA4_interaction_leads.sql
@@ -1,9 +1,11 @@
+# namastecita.analytics_322272818.events_2023*
+
 WITH sessions as (
 
   SELECT
   user_pseudo_id,
   CONCAT(user_pseudo_id,(select value.int_value from unnest(event_params) where key = 'ga_session_id')) as session_id
-  FROM # `add your ga events table`
+  FROM `namastecita.analytics_322272818.events_2023*`
 
 )
 
@@ -16,8 +18,8 @@ SELECT
  event_params.value.string_value,
  event_params.value.int_value
 
-FROM `` f ,unnest(event_params) event_params
+FROM `namastecita.analytics_322272818.events_2023*` f ,unnest(event_params) event_params
 
 JOIN sessions s ON f.user_pseudo_id=s.user_pseudo_id
 
-WHERE s.session_id IN (SELECT sessions FROM ``)
+WHERE s.session_id IN (SELECT CONCAT(user_pseudo_id,(select value.int_value from unnest(event_params) where key = 'ga_session_id')) as session_id FROM `namastecita.analytics_322272818.events_2023*`)
